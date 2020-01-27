d47b9781f2e3662d8acabb6265d91cd3bb725754
Nathan Brown <njbrown@ksu.edu>
2020-01-27 17:14:27 -0600

    Add project files.

---
+++ Alarm Clock/AddEditAlarm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Alarm_Clock
+{
+    partial class AddEditAlarm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxCancel = new System.Windows.Forms.Button();
+            this.uxSet = new System.Windows.Forms.Button();
+            this.uxTime = new System.Windows.Forms.DateTimePicker();
+            this.uxOnButton = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // uxCancel
+            // 
+            this.uxCancel.Location = new System.Drawing.Point(12, 71);
+            this.uxCancel.Name = "uxCancel";
+            this.uxCancel.Size = new System.Drawing.Size(75, 35);
+            this.uxCancel.TabIndex = 0;
+            this.uxCancel.Text = "Cancel";
+            this.uxCancel.UseVisualStyleBackColor = true;
+            // 
+            // uxSet
+            // 
+            this.uxSet.Location = new System.Drawing.Point(215, 71);
+            this.uxSet.Name = "uxSet";
+            this.uxSet.Size = new System.Drawing.Size(75, 35);
+            this.uxSet.TabIndex = 0;
+            this.uxSet.Text = "Set";
+            this.uxSet.UseVisualStyleBackColor = true;
+            this.uxSet.Click += new System.EventHandler(this.uxSet_Click);
+            // 
+            // uxTime
+            // 
+            this.uxTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.uxTime.Location = new System.Drawing.Point(12, 28);
+            this.uxTime.Name = "uxTime";
+            this.uxTime.Size = new System.Drawing.Size(200, 20);
+            this.uxTime.TabIndex = 1;
+            this.uxTime.Value = new System.DateTime(2020, 1, 24, 21, 25, 36, 0);
+            // 
+            // uxOnButton
+            // 
+            this.uxOnButton.AutoSize = true;
+            this.uxOnButton.Location = new System.Drawing.Point(218, 31);
+            this.uxOnButton.Name = "uxOnButton";
+            this.uxOnButton.Size = new System.Drawing.Size(39, 17);
+            this.uxOnButton.TabIndex = 2;
+            this.uxOnButton.TabStop = true;
+            this.uxOnButton.Text = "On";
+            this.uxOnButton.UseVisualStyleBackColor = true;
+            // 
+            // AddEditAlarm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 118);
+            this.Controls.Add(this.uxOnButton);
+            this.Controls.Add(this.uxTime);
+            this.Controls.Add(this.uxSet);
+            this.Controls.Add(this.uxCancel);
+            this.Name = "AddEditAlarm";
+            this.Text = "AddEditAlarm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uxCancel;
+        private System.Windows.Forms.Button uxSet;
+        private System.Windows.Forms.DateTimePicker uxTime;
+        private System.Windows.Forms.RadioButton uxOnButton;
+    }
+}
---
+++ Alarm Clock/AlarmClock.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Alarm_Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxEdit = new System.Windows.Forms.Button();
+            this.uxAdd = new System.Windows.Forms.Button();
+            this.uxAlarmList = new System.Windows.Forms.ListBox();
+            this.uxSnooze = new System.Windows.Forms.Button();
+            this.uxStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // uxEdit
+            // 
+            this.uxEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxEdit.Location = new System.Drawing.Point(12, 12);
+            this.uxEdit.Name = "uxEdit";
+            this.uxEdit.Size = new System.Drawing.Size(76, 46);
+            this.uxEdit.TabIndex = 0;
+            this.uxEdit.Text = "Edit";
+            this.uxEdit.UseVisualStyleBackColor = true;
+            // 
+            // uxAdd
+            // 
+            this.uxAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxAdd.Location = new System.Drawing.Point(150, 12);
+            this.uxAdd.Name = "uxAdd";
+            this.uxAdd.Size = new System.Drawing.Size(76, 46);
+            this.uxAdd.TabIndex = 0;
+            this.uxAdd.Text = "+";
+            this.uxAdd.UseVisualStyleBackColor = true;
+            // 
+            // uxAlarmList
+            // 
+            this.uxAlarmList.FormattingEnabled = true;
+            this.uxAlarmList.Location = new System.Drawing.Point(12, 104);
+            this.uxAlarmList.Name = "uxAlarmList";
+            this.uxAlarmList.Size = new System.Drawing.Size(214, 147);
+            this.uxAlarmList.TabIndex = 1;
+            // 
+            // uxSnooze
+            // 
+            this.uxSnooze.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxSnooze.Location = new System.Drawing.Point(12, 284);
+            this.uxSnooze.Name = "uxSnooze";
+            this.uxSnooze.Size = new System.Drawing.Size(76, 46);
+            this.uxSnooze.TabIndex = 0;
+            this.uxSnooze.Text = "Snooze";
+            this.uxSnooze.UseVisualStyleBackColor = true;
+            this.uxSnooze.Click += new System.EventHandler(this.uxSnooze_Click);
+            // 
+            // uxStop
+            // 
+            this.uxStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxStop.Location = new System.Drawing.Point(150, 284);
+            this.uxStop.Name = "uxStop";
+            this.uxStop.Size = new System.Drawing.Size(76, 46);
+            this.uxStop.TabIndex = 0;
+            this.uxStop.Text = "Stop";
+            this.uxStop.UseVisualStyleBackColor = true;
+            this.uxStop.Click += new System.EventHandler(this.uxStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(238, 353);
+            this.Controls.Add(this.uxAlarmList);
+            this.Controls.Add(this.uxAdd);
+            this.Controls.Add(this.uxStop);
+            this.Controls.Add(this.uxSnooze);
+            this.Controls.Add(this.uxEdit);
+            this.Name = "Form1";
+            this.Text = "Alarms";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uxEdit;
+        private System.Windows.Forms.Button uxAdd;
+        private System.Windows.Forms.ListBox uxAlarmList;
+        private System.Windows.Forms.Button uxSnooze;
+        private System.Windows.Forms.Button uxStop;
+    }
+}
+
